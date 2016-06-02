5390878810167e7b4c8bb83f93cbcae5d0137f14
Martin Vician <martin.vician@nic.cz>
2016-06-02 17:16:12 +0200

    about, deploy, blog menu

--- deploy.sh
+++ deploy.sh
@@ -9,5 +9,7 @@ ln -sf _config.local.yml _config.yml
 
 # VF
 rsync -avz -e ssh --delete ./_site/ vf.vician.cz:/var/www/vician.cz/
+# VF2
+rsync -avz -e ssh --delete ./_site/ vf2.vician.cz:/var/www/vician.cz/
 # CH
 rsync -avz -e ssh --delete ./_site/ ch.vician.cz:/var/www/vician.cz/
